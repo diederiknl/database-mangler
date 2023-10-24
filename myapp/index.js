6a1dffa64a5f2c6755c3b0aa223d0dd7fdd420e4
Diederik de Vries <d.de.vries@hr.nl>
2023-10-24 16:54:32 +0200

    First static website working

--- myapp/index.js
+++ myapp/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
+
+// Statische spullen: (https://expressjs.com/en/starter/static-files.html)
+//app.use(express.static("static"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+// app.post("/", (req, res) => {
+//   res.send("Got a POST request");
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
