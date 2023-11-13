ce32004ce381218bcfc3048b8c452fd86649d89b
Diederik de Vries <d.de.vries@hr.nl>
2023-11-13 20:24:40 +0100

    Lorum Ipsum toevoeging werkt. Nu file copy

--- myapp/index.js
+++ myapp/index.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
 const port = 8080;
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./myapp/database/bierendb.db');
 
 // Statische spullen: (https://expressjs.com/en/starter/static-files.html)
 //app.use(express.static("static"));
@@ -30,6 +32,27 @@ app.post("/", urlencodedParser, function (req, res) {
 
   // Ok. Dus array.indexOf kunnen we gebruiken om
   if (array.includes(studentnummer) === true) {
+
+    db.serialize(() => {
+      db.run("CREATE TABLE lorem (info TEXT)");
+  
+      const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+      console.log(stmt);
+      for (let i = 0; i < 10; i++) {
+          stmt.run("Ipsum " + i);
+      }
+      stmt.finalize();
+  
+      // db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
+      //     console.log(row.id + ": " + row.info);
+      // });
+      });
+  
+    db.close();
+
+
+
+
     res.render("index", { title: "Hey", message: "Gelukt!" });
   } else {
     res.render("index", { title: "Hey", message: "Mislukt!" });
