4c475e5ac8aab4f4f682927e2e8d497adc62fdb8
Diederik de Vries <d.de.vries@hr.nl>
2023-11-13 20:32:07 +0100

    Copieeren van database voor mangelen werkt.

--- myapp/index.js
+++ myapp/index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 const port = 8080;
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./myapp/database/bierendb.db');
+//const db = new sqlite3.Database('./myapp/database/bierendb.db');
 
 // Statische spullen: (https://expressjs.com/en/starter/static-files.html)
 //app.use(express.static("static"));
@@ -33,24 +33,28 @@ app.post("/", urlencodedParser, function (req, res) {
   // Ok. Dus array.indexOf kunnen we gebruiken om
   if (array.includes(studentnummer) === true) {
 
-    db.serialize(() => {
-      db.run("CREATE TABLE lorem (info TEXT)");
+    // db.serialize(() => {
+    //   db.run("CREATE TABLE lorem (info TEXT)");
   
-      const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-      console.log(stmt);
-      for (let i = 0; i < 10; i++) {
-          stmt.run("Ipsum " + i);
-      }
-      stmt.finalize();
+    //   const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+    //   console.log(stmt);
+    //   for (let i = 0; i < 10; i++) {
+    //       stmt.run("Ipsum " + i);
+    //   }
+    //   stmt.finalize();
   
-      // db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
-      //     console.log(row.id + ": " + row.info);
-      // });
-      });
+    //   // db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
+    //   //     console.log(row.id + ": " + row.info);
+    //   // });
+    //   });
   
-    db.close();
+    // db.close();
 
-
+    // Database copieeren vóór mangelen
+    fs.copyFile('./myApp/database/bierendb.db', './myApp/database/'+studentnummer+'.db', (err) => {
+      if (err) throw err;
+      console.log('Database copied ')
+    });
 
 
     res.render("index", { title: "Hey", message: "Gelukt!" });
